bfde7dbec3d1052027223f990c86f6a14ef62535
leandropaulino <leandropaulino@brq.com>
2021-07-18 17:17:54 -0300

    Atualizao no administardor para suportar a criacao de usuarios

--- app/core/models.py
+++ app/core/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
         if not email:
-            raise ValueError('Usuarios precisam possuir um endereço de email')
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
